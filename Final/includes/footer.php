ae9af550bc1f15c5572372e3eb101d4c0f7ad5a7
Akash Saha <50497930+AkashRockz@users.noreply.github.com>
2019-11-25 23:37:29 +0600

    Add files via upload

---
+++ Final/includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+      </div>
+      <strong>Copyright:Akash Saha<br>Powered By:Digital World Site
+      
+        </strong>
+    </div>
+</footer>
